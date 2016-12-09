21cee0e2bc81991e8c1515df6d75f007a906684d
Ivan <goloskokovic@gmail>
2016-12-09 18:09:50 +0100

    added ThreadSafeClientConnManager

--- src/main/java/com/wire/bots/propeller/ImageRecognitionClient.java
+++ src/main/java/com/wire/bots/propeller/ImageRecognitionClient.java
@@ -1,21 +1,19 @@
 package com.wire.bots.propeller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.config.SocketConfig;
-import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
-
-import java.io.IOException;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
 import java.net.URI;
-import java.net.URISyntaxException;
-
-import com.wire.wbotz.Logger;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -26,14 +24,18 @@ public class ImageRecognitionClient {
 
     private ObjectMapper objectMapper;
     private CloseableHttpClient client;
-    private final static int SO_TIMEOUT = 60 * 1000;
+    private final static int SO_TIMEOUT = 60;
     private final String apiUrl = "http://104.196.212.163:8000/api/classify/image";
 
     public ImageRecognitionClient() {
-        SocketConfig socketConfig = SocketConfig.custom().setSoTimeout(SO_TIMEOUT).build();
-        HttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager();
-        ((BasicHttpClientConnectionManager) connectionManager).setSocketConfig(socketConfig);
-        client = HttpClientBuilder.create().setConnectionManager(connectionManager).build();
+        HttpParams params = new BasicHttpParams();
+        SchemeRegistry registry = new SchemeRegistry();
+        registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 8000));
+        ThreadSafeClientConnManager cm = new ThreadSafeClientConnManager(params, registry);
+        cm.setDefaultMaxPerRoute(10);
+        cm.setMaxTotal(100);
+        cm.closeIdleConnections(SO_TIMEOUT, TimeUnit.SECONDS);
+        CloseableHttpClient client = new DefaultHttpClient(cm, params);
         objectMapper = new ObjectMapper();
     }
 
@@ -45,6 +47,7 @@ public class ImageRecognitionClient {
         CloseableHttpResponse response = client.execute(httpRequest);
         ImageClassificationResponse classification =
                 objectMapper.readValue(response.getEntity().getContent(), ImageClassificationResponse.class);
+        response.getEntity().getContent().close();
         response.close();
 
         return classification;
